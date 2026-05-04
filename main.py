e4f2ed1b4d431d04d86715fa6dc718e70fb12b1f
Alef Santos <aleflameirasantos2507@gmail.com>
2026-05-04 19:14:18 -0300

    Fix import for TfidfTransformer to TfidfVectorizer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
